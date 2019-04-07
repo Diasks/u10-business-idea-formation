5f34e41870bda1cf387710e51658206dfd85cd03
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-04-07 21:29:50 +0200

    add routes for crud users

--- Formation/routes/users.js
+++ Formation/routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET users  */
+router.get('/', function(req, res) {
+  res.json({"status": "OK", "data" : ""});
+});
+
+
+/* GET  specific user by ID  */
+router.get('/', function(req, res) {
+  res.json({"status": "OK", "data" : ""});
+});
+
+/* POST create user  */
+router.post('/', function(req, res) {
+  res.json({"status": "OK", "data" : `recieved, ${req.body.name}`});
+});
+
+/* PUT update user  */
+router.put('/:userId', function(req, res) {
+  res.json({"status": "OK", "data" : `Put request, ${req.params.userId}`});
+});
+
+/* DELETE user listing */
+router.delete('/:userId', function(req, res) {
+  res.json({"status": "OK", "data" : `Delete request, ${req.params.userId}`});
 });
 
 module.exports = router;
