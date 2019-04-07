02c1c135c183fc84e215ec45c9f8108b3c87b47f
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-04-08 01:08:06 +0200

    add register, profile, login

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
-
-export default App;
+import React, { Component } from 'react';			
+			import { BrowserRouter as Router, Route } from 'react-router-dom';
+			import Header from './components/layout/Header';
+			import Profile from './components/pages/Profile';
+			import Login from './components/pages/Login';
+			import Register from './components/pages/Register';
+			import './App.css';
+			
+			class App extends Component {
+			
+			
+			
+			
+			render() {
+			return (
+			<Router>
+			<div className="App">
+			<div className="container">
+			<Header />
+			<Route  path="/login" component={Login}/>
+			<Route exact path ="/about" component= {Profile} />
+			<Route exact path ="/register" component= {Register} />
+			</div>
+			</div>
+			</Router>
+			
+			);
+			}
+			}
+			
+			export default App;
