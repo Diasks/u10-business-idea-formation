45121d53e7d89d8fad6bfead0891812344aeb20e
oksanterova <oksana.kanterova@gmail.com>
2019-04-14 21:04:46 +0200

    Add Welcome component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,39 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Profile from "./components/pages/Profile";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+import Footer from "./components/layout/Footer";
+import Welcome from "./components/Welcome";
+import "./App.css";
 
-import React, { Component } from 'react';			
-			import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
-			import PageTwo from './components/pages/PageTwo';
-			import './App.css';
-			
-			class App extends Component {
-			
-			
-			
-			
-			render() {
-			return(
-				<Router> 
-				<div className="formtitle">
-				<NavLink to="/PageTwo">Start here</NavLink> 
-				</div>
-				<Route exact path="/PageTwo" component={PageTwo}>
-        </Route>
-				</Router>
-			)
-			}
-			}
-		
-			export default App;
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div className="formtitle">
+            <NavLink to="/PageTwo">Start here</NavLink>
+          </div>
+          <Route exact path="/PageTwo" component={PageTwo} />
+
+          <div>
+            {
+              // TODO
+              //<Header />
+              //<Route path="/login" component={Login} />
+              //<Route exact path="/profile" component={Profile} />
+              //<Route exact path="/register" component={Register} />
+              //<Footer />
+            }
+            <Welcome />
+            <Footer />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,27 +1,25 @@
-import React from 'react';			
-            import { Container} from 'react-bootstrap';
-			
-			function Footer() {
-			return (
-                <footer style={footerStyle}>
-                <Container>
-                
-          
-                  <div className="text-center small copyright">
-                    © FORMATION 2018
-                  </div>
-                </Container>
-              </footer>
-            );
-            }
-            
-            const  footerStyle = {
-                background: '#333',
-                color: '#fff',
-                textAlign: 'center',
-                padding: '10px'
-                }
-                
+import React from "react";
+import { Container } from "react-bootstrap";
 
-           
-			export default Footer;
+function Footer() {
+  return (
+    <footer style={footerStyle}>
+      <Container>
+        <h5>
+          <strong>Connect with us!</strong>
+        </h5>
+
+        <div className="text-center small copyright">© FORMATION 2018</div>
+      </Container>
+    </footer>
+  );
+}
+
+const footerStyle = {
+  background: "#55273c",
+  color: "#fff",
+  textAlign: "center",
+  padding: "10px"
+};
+
+export default Footer;
