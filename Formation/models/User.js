bac140eae21f97c497380cffee00d2996ec0a2eb
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-04-20 21:38:12 +0200

    add styling to loginform and registerform

--- Formation/models/User.js
+++ Formation/models/User.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+
 var UserSchema = new Schema({
+    _id: mongoose.Schema.Types.ObjectId,
     first_name: String,
     last_name: String,
     password: String,
