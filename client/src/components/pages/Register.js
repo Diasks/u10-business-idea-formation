62547cba58436ed1ec80396f3a9007ab6383ab6c
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-04-20 21:39:47 +0200

    add classnames and validation for form and  inputfields

--- client/src/components/pages/Register.js
+++ client/src/components/pages/Register.js
@@ -1,6 +1,20 @@
-import React, { Component } from 'react';			
+import React, { Component } from 'react';	
+
 			import axios from 'axios';
+		
+
+			var emailRegex = RegExp(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
+
 			
+var formValid = formErrors => {
+	var valid = true;
+
+	Object.values(formErrors).forEach( val => {
+		val.length > 0 && (valid=false);
+	});
+	return valid;
+};
+
 			
 			export class Register extends Component {
 			constructor() {
@@ -11,7 +25,13 @@ import React, { Component } from 'react';
 			first_name: '',
 			last_name: '',
 			password: '',
-			email: ''
+			email: '',
+			formErrors: {
+				first_name: '',
+			last_name: '',
+			password: '',
+			email: '',
+			}
 			};
 			
 			
@@ -23,12 +43,36 @@ import React, { Component } from 'react';
 			
 			
 			handleChange(e) {
+				e.preventDefault();
 			let target = e.target;
 			let value = target.value;
 			let name = target.name;
+			let formErrors = this.state.formErrors;
+
+			switch(name) {
+				case 'first_name':
+				formErrors.first_name = value.length < 3  ? 'minimum 3 characters required, only letters'
+				: '';
+				break;
+				case 'last_name': 
+				formErrors.last_name = value.length < 3  ? 'minimum 3 characters required'
+				: '';
+				break;
+				case 'email': 
+				formErrors.email = emailRegex.test(value)  ? ''
+				: "invalid email address";
+				break;
+				case 'password': 
+				formErrors.password = value.length < 6  ? 'minimum 3 characters required'
+				: '';
+				break;
+				default:
+				break;
+
+			}
 			
 			
-			this.setState({
+			this.setState({formErrors,
 			[name]: value
 			
 			});
@@ -41,6 +85,8 @@ import React, { Component } from 'react';
 			console.log('submitted');
 			console.log(this.state);
 			
+			if (formValid(this.state.formErrors)) { 
+
 			const user = {
 			first_name: this.state.first_name,
 			last_name: this.state.last_name,
@@ -55,48 +101,62 @@ import React, { Component } from 'react';
 			}).catch(err => {
 				err.status(400).send('unable to save to database');
 			});
-			
+		}else {console.error('Form invalid!')};
 			
 			}
 			
 			
 			render () {
-			
+			var {formErrors} = this.state;
 			return (
 			
-			<div>
-			<form onSubmit={this.handleSubmit}>
-			<div>
-			<label htmlFor="first_name">Firstname</label>
-			<input type="text" id="first_name" placeholder="enter your firstname" name="first_name" value={this.state.first_name} onChange={this.handleChange}/>
-			
+			<div className="Form">
+			<form onSubmit={this.handleSubmit} className="FormInputs">
+			<div className="form-group">
+			<label htmlFor="first_name" className="FormInputLabel">Firstname</label>
+			<input type="text" className="form-control" id="first_name" placeholder="enter your firstname" name="first_name" value={this.state.first_name} onChange={this.handleChange}/>
+			{formErrors.first_name.length >0 && (
+				<span className="errorMessage">{formErrors.first_name}</span>
+			)}
 			
 			</div>
 
-			<div>
-			<label htmlFor="last_name">Lastname</label>
-			<input type="text" id="last_name" placeholder="enter your lastname" name="last_name" value={this.state.last_name} onChange={this.handleChange}/>
+			<div className="form-group">
+			<label htmlFor="last_name" className="FormInputLabel">Lastname</label>
+			<input type="text" className="form-control" id="last_name" placeholder="enter your lastname" name="last_name" value={this.state.last_name} onChange={this.handleChange}/>
+			{formErrors.last_name.length >0 && (
+				<span className="errorMessage">{formErrors.last_name}</span>
+			)}
+			
+			</div>
+			
+			<div className="form-group">
+			<label htmlFor="password" className="FormInputLabel">Password</label>
+			<input type="password" className="form-control" id="password" placeholder="enter your full name" name="password" value={this.state.password} onChange={this.handleChange} />
+			{formErrors.password.length >0 && (
+				<span className="errorMessage">{formErrors.password}</span>
+			)}
+			
+			</div>
+			
+			
+			<div className="form-group">
+			<label htmlFor="email" className="FormInputLabel">Email</label>
+			<input type="email" className="form-control" id="email" placeholder="enter your full name" name="email" value={this.state.email} onChange={this.handleChange}/>
+			{formErrors.email.length >0 && (
+				<span className="errorMessage">{formErrors.email}</span>
+			)}
 			
 			
 			</div>
 			
 			<div>
-			<label htmlFor="password">Password</label>
-			<input type="password" id="password" placeholder="enter your full name" name="password" value={this.state.password} onChange={this.handleChange} />
+
+				<button type="submit" className="FormButton">Register</button>  
 			
-			
-			</div>
-			
-			
-			<div>
-			<label htmlFor="email">Email</label>
-			<input type="email" id="email" placeholder="enter your full name" name="email" value={this.state.email} onChange={this.handleChange}/>
-			
-			
-			
-			</div>
-			
-			<div><button>Register</button></div>
+	
+       
+				</div>
 			</form>
 			</div>
 			
