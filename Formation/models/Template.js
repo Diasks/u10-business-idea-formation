84a82eb299f65624dd5854c29a259f13cec1447b
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-04-23 19:12:26 +0200

    add moongose object id for templateschema

--- Formation/models/Template.js
+++ Formation/models/Template.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var TemplateSchema = new Schema({
+    _id: mongoose.Schema.Types.ObjectId,
     first_name: String,
     last_name: String,
     email: String,
