a459773352b48255a23618e43a1c370dc884e5a2
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-04-24 22:22:51 +0200

    fix pug-error

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
 
 import React, { Component } from 'react';			
-			import { BrowserRouter as Router, Route } from 'react-router-dom';
-			import Header from './components/layout/Header';
-			import Profile from './components/pages/Profile';
-			import Login from './components/pages/Login';
-			import Register from './components/pages/Register';
-			import Footer from './components/layout/Footer'
+			import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+			import PageTwo from './components/pages/PageTwo';
 			import './App.css';
 			
 			class App extends Component {
@@ -14,23 +10,15 @@ import React, { Component } from 'react';
 			
 			
 			render() {
-			return (
-			<Router>
-			<div className="App">
-			<div className="container">
-			<Header />
-			<Route  path="/login" component={Login}/>
-			<Route exact path ="/profile" component= {Profile} />
-			<Route exact path ="/register" component= {Register} />
-			<div id="content">
-          
-		  </div>
-			<Footer />
-			</div>
-			</div>
-			</Router>
-					
-			);
+			return(
+				<Router> 
+				<div className="formtitle">
+				<NavLink to="/PageTwo">Start here</NavLink> 
+				</div>
+				<Route exact path="/PageTwo" component={PageTwo}>
+        </Route>
+				</Router>
+			)
 			}
 			}
 		
--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,98 +1,93 @@
-import React, { Component } from 'react';			
-			import axios from 'axios';
-			
-			
-			export class Login extends Component {
-			constructor() {
-			super();
-			
-			
-			this.state = {
-			email: '',
-			password: '',
-			
-			};
-			
-			
-			this.handleChange = this.handleChange.bind(this);
-			this.handleSubmit = this.handleSubmit.bind(this);
-			
-			
-			}
-			
-			
-			handleChange(e) {
-			let target = e.target;
-			let value = target.value;
-			let name = target.name;
-			
-			
-			this.setState({
-			[name]: value
-			
-			});
-			
-			}
-			
-			//Kopplad till backenden(!!)
-			handleSubmit(e) {
-			e.preventDefault();
-			console.log('submitted');
-			console.log(this.state);
-			
-			const user = {
-			email: this.state.email,
-			password: this.state.password
-			};
-			
-			axios.get(`http://localhost:4000/users`, { user })
-			.then(res => {
+import React, { Component } from "react";
+import axios from "axios";
+
+export class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    let target = e.target;
+    let value = target.value;
+    let name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  //Kopplad till backenden(!!)
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log("submitted");
+    console.log(this.state);
+
+    const user = {
+      email: this.state.email,
+      password: this.state.password
+    };
+
+	axios.post(`http://localhost:4000/api/login`, user)
+		.then(res => {
+			debugger;
 			console.log(res);
 			console.log(res.data);
-			})
-			
-			
-			}
-			
-			
-			render () {
-			
-			return (
-			
-			<div>
-			<form onSubmit={this.handleSubmit}>
-			
-			<div>
-			<label htmlFor="email">Email</label>
-			<input type="email" id="email" placeholder="enter your full name" name="email" value={this.state.email} onChange={this.handleChange}/>
-			
-			
-			
-			</div>
-			
-			<div>
-			<label htmlFor="password">Password</label>
-			<input type="password" id="password" placeholder="enter your full name" name="password" value={this.state.password} onChange={this.handleChange} />
-			
-			
-			</div>
-			
-			
-			
-			
-			<div><button>Logga in</button></div>
-			</form>
-			</div>
-			
-			)}
-			}
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			export default Login;
+			localStorage.setItem("cool-jwt", res.data.token);
+			localStorage.setItem("user", res.data.user._id);
+    	});
+  }
+
+  render() {
+    return (
+      <div className="Form">
+        <form onSubmit={this.handleSubmit} className="FormInputs">
+          <div className="form-group">
+            <label htmlFor="email" className="FormInputLabel">
+              Email
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="enter your full name"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password" className="FormInputLabel">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="enter your full name"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div>
+            <button type="submit" className="FormButton">
+              Sign in
+            </button>{" "}
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
