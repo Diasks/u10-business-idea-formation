c990aae1118bd336706697c09e1127fe3465a5af
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-04-24 22:21:58 +0200

    get request, search database, send back token and user

--- Formation/app.js
+++ Formation/app.js
@@ -1,49 +1,88 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var config = require('dotenv').config();
-var mongoose = require('mongoose');
-var cors = require('cors');
-
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
+var config = require("dotenv").config();
+var mongoose = require("mongoose");
+var cors = require("cors");
+var jwt = require("jsonwebtoken");
+var User = require("./models/User");
+var pug = require("pug");
 
 var app = express();
 app.use(cors());
 
 //Connectar till mongoose utan problem
-mongoose.connect(`mongodb+srv://${process.env.MLAB_USERNAME}:${process.env.MLAB_PASSWORD}@formation-db-xw7ax.mongodb.net/test?retryWrites=true
+mongoose.connect(`mongodb+srv://${process.env.MLAB_USERNAME}:${
+  process.env.MLAB_PASSWORD
+}@formation-db-xw7ax.mongodb.net/test?retryWrites=true
 `);
 
 var db = mongoose.connection;
-db.on('error', function() {
- console.error('whoops, something went wrong!');
+
+db.on("error", function() {
+  console.error("whoops, something went wrong!");
 });
-db.once('open', function() {
-  console.log('DB connection is up an running');
+db.once("open", function() {
+  console.log("DB connection is up an running");
 });
 
-
-
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var templatesRouter = require("./routes/templates");
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.post("/api/login", function(req, res) {
+  //auth user
+  debugger;
+  console.log(req.body);
+  console.log(req.body.email);
+  debugger;
+  User.findOne({email: req.body.email}, function(err, user) {
+    debugger;
+    console.log(user);
+    if (!err) {
+      debugger;
+      var token = jwt.sign(user.toJSON(), process.env.SECRET_OR_KEY,  { expiresIn: '24h' // expires in 24 hours
+      });
+   res.json({
+      success: true,
+      message: 'Authentication successfull',
+      token: token,
+      user: user
+    });
+    } else {
+      console.log('error');
+    }
+    // user exists, create token and return to client
+    // done
+  })
+  
+  // var user = {id: 3};
+  //   var token = jwt.sign(user, process.env.SECRET_OR_KEY,  { expiresIn: '24h' // expires in 24 hours
+  // });
+  //   res.json({
+  //     success: true,
+  //     message: 'Authentication successfull',
+  //     token: token,
+  //     user: user
+  //   });
+});
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/templates", templatesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -54,12 +93,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
-
