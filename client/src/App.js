ba0d69ff8262091fdbdd442cd241e027208e84d1
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-04-25 18:02:04 +0200

    add link temporarely to get to professioncomponent from my startpage

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';			
 			import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 			import PageTwo from './components/pages/PageTwo';
+			import Profession from './components/pages/Profession';
 			import './App.css';
 			
 			class App extends Component {
@@ -14,8 +15,11 @@ import React, { Component } from 'react';
 				<Router> 
 				<div className="formtitle">
 				<NavLink to="/PageTwo">Start here</NavLink> 
+				<NavLink to="/Profession">Yrken</NavLink> 
 				</div>
 				<Route exact path="/PageTwo" component={PageTwo}>
+        </Route>
+		<Route exact path="/Profession" component={Profession}>
         </Route>
 				</Router>
 			)
