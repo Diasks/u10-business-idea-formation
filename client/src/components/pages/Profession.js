7de4db1e3a985579a7dc2b3366f682b6222f597f
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-04-25 18:24:29 +0200

    fix route for professionbuttons on click that will send user to another component

--- client/src/components/pages/Profession.js
+++ client/src/components/pages/Profession.js
@@ -1,50 +1,41 @@
-import React from 'react';	
+import React, {Component} from 'react';	
+            
+class Profession extends Component {
+constructor() {
+    super();
+    this.routeChange = this.routeChange.bind(this);
+}
 
-			
-			function Profession() {
+routeChange() {
+    var path = `PageTwo`;
+    this.props.history.push(path);
+}
+ 
+render() {
 			return (
                 <div> 
-            
-	
-
             <div class="jumbotron jumbotron-fluid">
   <div class="container">
     <h1 class="display-4">	
 			Choose a profession:</h1>
-    <button type="submit" className="ProfessionButton">Hantverksyrken</button>  
-            <button type="submit" className="ProfessionButton">Serviceyrken</button>  
-            <button type="submit" className="ProfessionButton">Socialt arbete</button>  
-            <button type="submit" className="ProfessionButton">Tekniskt arbete</button>  
-            <button type="submit" className="ProfessionButton">Pedagogiskt arbete</button>  
-            <button type="submit" className="ProfessionButton">Vård</button> 
+    <button type="submit" className="ProfessionButton" onClick={this.routeChange}>Hantverksyrken</button>  
+            <button type="submit" className="ProfessionButton" onClick={this.routeChange}>Serviceyrken</button>  
+            <button type="submit" className="ProfessionButton" onClick={this.routeChange}>Socialt arbete</button>  
+            <button type="submit" className="ProfessionButton" onClick={this.routeChange}>Tekniskt arbete</button>  
+            <button type="submit" className="ProfessionButton" onClick={this.routeChange}>Pedagogiskt arbete</button>  
+            <button type="submit" className="ProfessionButton" onClick={this.routeChange}>Vård</button> 
 
-            <button type="submit" className="ProfessionButton">Kultur</button>  
-            <button type="submit" className="ProfessionButton">
+            <button type="submit" className="ProfessionButton" onClick={this.routeChange}>Kultur</button>  
+            <button type="submit" className="ProfessionButton" onClick={this.routeChange}>
 Naturvetenskapligt arbete</button>  
-            <button type="submit" className="ProfessionButton">Sanering och renhållning</button> 
-            <button type="submit" className="ProfessionButton">Transport</button> 
+            <button type="submit" className="ProfessionButton" onClick={this.routeChange}>Sanering och renhållning</button> 
+            <button type="submit" className="ProfessionButton" onClick={this.routeChange}>Transport</button> 
   </div>
 </div>
-           
-
-
-
-			<p>Här vill jag displaya massa yrken!
-
-
-
-
-
-</p>
-
             </div>
-      
-       
-            )
-    
-       
-            }
+      )}
             
+            }
             
             export default Profession;
             
