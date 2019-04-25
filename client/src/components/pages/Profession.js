2c11645239e9bf2528a48a2e8cc9a35d6ae9ca4a
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-04-25 18:03:04 +0200

    add jumbotron, h1 and buttons with professions inside

--- client/src/components/pages/Profession.js
+++ client/src/components/pages/Profession.js
@@ -1,12 +1,51 @@
-import React from 'react';			
+import React from 'react';	
+
 			
 			function Profession() {
 			return (
-		<div> 
-			<h1>Yrken!!</h1>
-			<p>Här vill jag displaya massa yrken!</p>
+                <div> 
+            
+	
+
+            <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">	
+			Choose a profession:</h1>
+    <button type="submit" className="ProfessionButton">Hantverksyrken</button>  
+            <button type="submit" className="ProfessionButton">Serviceyrken</button>  
+            <button type="submit" className="ProfessionButton">Socialt arbete</button>  
+            <button type="submit" className="ProfessionButton">Tekniskt arbete</button>  
+            <button type="submit" className="ProfessionButton">Pedagogiskt arbete</button>  
+            <button type="submit" className="ProfessionButton">Vård</button> 
+
+            <button type="submit" className="ProfessionButton">Kultur</button>  
+            <button type="submit" className="ProfessionButton">
+Naturvetenskapligt arbete</button>  
+            <button type="submit" className="ProfessionButton">Sanering och renhållning</button> 
+            <button type="submit" className="ProfessionButton">Transport</button> 
+  </div>
+</div>
+           
+
+
+
+			<p>Här vill jag displaya massa yrken!
+
+
+
+
+
+</p>
+
             </div>
-			)
-			}
-			
-			export default Profession;
+      
+       
+            )
+    
+       
+            }
+            
+            
+            export default Profession;
+            
+        
