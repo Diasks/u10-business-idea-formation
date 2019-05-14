d2a1fb11ea2a1561bd3cb86d753061c1c75dbede
Diana Skshipek <Diana.Skshipek@chasacademy.se>
2019-05-14 14:00:34 +0200

    connect backend to frontend via express

--- Formation/routes/index.js
+++ Formation/routes/index.js
@@ -1,11 +1,14 @@
 
 var express = require('express');
 var router = express.Router();
-
+const app = express();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendFile("../../client/build/index.html");
-});
+// router.get('/', function(req, res, next) {
+//   res.sendFile("../../client/build/index.html");
+
+// });
+
+app.use(express.static("../../client/build"));
 
 module.exports = router;
