562fa26556bcaa7a5b867211b94ca997748eb55c
Astrid <astrid.sinabian@chasacademy.se>
2019-05-20 13:37:49 +0200

    Add structure to CV template

--- client/src/components/pages/GenTemplate.js
+++ client/src/components/pages/GenTemplate.js
@@ -7,14 +7,65 @@ import jsPDF from 'jspdf';
 
 const Body = styled.div`
  max-height: 900px;
+ max-width: 700px;
  margin: 30px auto;
  padding: 30px;
  height: 100%;
  width: 100%;
 `;
 
-const FullName = styled.h1`
- color: red;
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  
+`;
+
+const FullName = styled.div`
+ font-size: 10px;
+`;
+
+const Place = styled.div`
+  font-size: 10px;
+`;
+
+const Telephone = styled.div`
+  font-size: 10px;
+`;
+
+const Email = styled.div`
+  font-size: 10px;
+`;
+
+const CvHeader = styled.h1`
+
+`; 
+
+const Contacts = styled.div`
+  
+`;
+
+const WorkExperienceWrapper = styled.div`
+  margin: 40px 0 20px 0;
+`;
+
+const EducationWrapper = styled.div`
+  margin: 20px 0 20px 0;
+`;
+
+const OthersWrapper = styled.div`
+  margin: 20px 0 20px 0;
+`;
+
+const SkillsWrapper = styled.div`
+  margin: 20px 0 20px 0;
+`;
+
+const ReferencesWrapper = styled.div`
+ margin: 20px 0 20px 0;
+`;
+
+const Reference = styled.div`
+  font-size: 12px;
 `;
 
 const Print = styled.button``;
@@ -55,9 +106,42 @@ function GenTemplate() {
    <div>
      <Print onClick={e => handlePrint(user)}>Print</Print>
      <Body id="body">
-       <FullName>
-         {user.first_name} {user.last_name}
-       </FullName>
+
+       <HeaderWrapper>
+          <CvHeader>CV</CvHeader>
+          <Contacts>
+            <FullName>{user.first_name} {user.last_name}</FullName>
+            <Place>{user.place}</Place>
+            <Telephone>{user.telephone}</Telephone>
+            <Email>{user.email}</Email>
+          </Contacts>
+       </HeaderWrapper>
+
+       <WorkExperienceWrapper>
+          <h6>Arbetslivserfarenhet</h6>
+
+       </WorkExperienceWrapper>
+
+       <EducationWrapper>
+          <h6>Utbildning</h6>
+
+       </EducationWrapper>
+
+       <SkillsWrapper>
+          <h6>Kompetens</h6>
+
+       </SkillsWrapper>
+
+       <OthersWrapper>
+          <h6>Övriga meriter</h6>
+
+       </OthersWrapper>
+
+       <ReferencesWrapper>
+          <h6>Referenser</h6>
+          <Reference>Lämnas på begäran</Reference>
+       </ReferencesWrapper>
+       
      </Body>
    </div>
  );
