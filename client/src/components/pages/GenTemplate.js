e78fa6a4bcd231b42a99a29157fe2fc698aa0717
Astrid <astrid.sinabian@chasacademy.se>
2019-05-20 18:25:48 +0200

    CV Template design/structure almost done

--- client/src/components/pages/GenTemplate.js
+++ client/src/components/pages/GenTemplate.js
@@ -20,8 +20,20 @@ const HeaderWrapper = styled.div`
   
 `;
 
-const FullName = styled.div`
- font-size: 10px;
+const CvHeader = styled.h1`
+  
+`; 
+
+const Contacts = styled.div`
+  
+`;
+
+const FirstName = styled.div`
+  
+`;
+
+const LastName = styled.div`
+  font-weight: bold;
 `;
 
 const Place = styled.div`
@@ -36,39 +48,82 @@ const Email = styled.div`
   font-size: 10px;
 `;
 
-const CvHeader = styled.h1`
-
-`; 
-
-const Contacts = styled.div`
-  
+const ObjectiveWrapper = styled.div`
+  font-size: 10px;
+  width: 70%;
 `;
+
 
 const WorkExperienceWrapper = styled.div`
   margin: 40px 0 20px 0;
 `;
 
+const WorkWrapper = styled.ul`
+  padding: 0;
+  margin-top: 20px;
+`;
+
+const Work = styled.li`
+  list-style: none;
+  margin-top: 10px;
+`;
+
+const Role = styled.div`
+  font-size: 12px;
+`;
+
+const Date = styled.div`
+  font-size: 12px;
+  font-style: italic;
+`;
+
 const EducationWrapper = styled.div`
-  margin: 20px 0 20px 0;
+  margin: 35px 0 20px 0;
+`;
+
+const Education = styled.li`
+  list-style: none;
+  margin-top: 10px;
+`;
+
+const Program = styled.div`
+  font-size: 12px;
+  margin-top: 10px;
+`;
+
+const EdDate = styled.div`
+  font-size: 12px;
+  font-style: italic;
 `;
 
 const OthersWrapper = styled.div`
-  margin: 20px 0 20px 0;
+  margin: 35px 0 20px 0;
+`;
+
+const Other = styled.div`
+  font-size: 12px;
 `;
 
 const SkillsWrapper = styled.div`
-  margin: 20px 0 20px 0;
+  margin: 35px 0 20px 0;
+`;
+
+const Skill = styled.div`
+  font-size: 12px;
 `;
 
 const ReferencesWrapper = styled.div`
- margin: 20px 0 20px 0;
+ margin: 35px 0 20px 0;
 `;
 
 const Reference = styled.div`
   font-size: 12px;
+  font-style: italic;
 `;
 
-const Print = styled.button``;
+const Print = styled.button`
+  
+`;
 
 function handlePrint(user, quality = 1) {
  const filename = `${user.first_name}_${user.last_name}_CV.pdf`;
@@ -102,43 +157,84 @@ function GenTemplate() {
    );
  }
 
+ console.log(user);
+
  return (
    <div>
      <Print onClick={e => handlePrint(user)}>Print</Print>
      <Body id="body">
 
        <HeaderWrapper>
-          <CvHeader>CV</CvHeader>
+          <CvHeader>
+            <FirstName>{user.first_name.toUpperCase()}</FirstName>
+            <LastName>{user.last_name.toUpperCase()}</LastName>
+          </CvHeader>
           <Contacts>
-            <FullName>{user.first_name} {user.last_name}</FullName>
+            
             <Place>{user.place}</Place>
             <Telephone>{user.telephone}</Telephone>
             <Email>{user.email}</Email>
           </Contacts>
        </HeaderWrapper>
 
+       <ObjectiveWrapper>
+         <p>{user.objective}</p>
+       </ObjectiveWrapper>
+
        <WorkExperienceWrapper>
           <h6>Arbetslivserfarenhet</h6>
+          <hr />
+          <WorkWrapper>
+            {user.jobs.map(job =>
+            <Work key={job.id}>
+              <Role>{job.role}</Role>
+              <Date>{job.start_date} - {job.end_date}</Date>
+            </Work>
+            )}
+          </WorkWrapper>
 
        </WorkExperienceWrapper>
 
        <EducationWrapper>
           <h6>Utbildning</h6>
+          <hr />
+          
+          {/* Tillfälligt med fake-data*/}
+          <Program>Företagsekonomi, Stockholms Universitet, Stockholm</Program>
+          <EdDate>2013-08-01 - 2016-06-01</EdDate>
+          <Program>Samhällsvetenskapliga programmet, Fyrisskolan, Uppsala</Program>
+          <EdDate>2010-08-01 - 2013-06-01</EdDate>
+
+          <ul>
+            {user.schools.map(school =>
+            <Education key={school.id}>
+              {/* <p>{school.program}</p> */}
+              
+            </Education>
+            )}
+          </ul>
 
        </EducationWrapper>
 
        <SkillsWrapper>
           <h6>Kompetens</h6>
+          <hr />
+
+          <Skill>{user.skills}</Skill>
 
        </SkillsWrapper>
 
        <OthersWrapper>
           <h6>Övriga meriter</h6>
+          <hr />
+
+          <Other>{user.others}</Other>
 
        </OthersWrapper>
 
        <ReferencesWrapper>
           <h6>Referenser</h6>
+          <hr />
           <Reference>Lämnas på begäran</Reference>
        </ReferencesWrapper>
        
