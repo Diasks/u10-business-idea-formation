17fde34d9b8c27abcd566069e2c61fa13571695c
Diego Pinones <pinones.diego@gmail.com>
2019-05-26 14:11:13 +0200

    fixed messageform buttons

--- client/src/components/pages/MessageForm.js
+++ client/src/components/pages/MessageForm.js
@@ -9,11 +9,25 @@ const StyleHeader1 = styled.h1`
   font-size: 40px;
   text-align: center;
 `;
+
+// blue button
 const FormButton = styled.form`
-  position: absolute;
-  right: 0;
-  margin-top: -35px;
+position: relative;
+top: -37px;
+left: 700px;
+  
 `;
+// red button
+const FormButton2 = styled.form`
+position:relative;
+transition: .5s ease;
+top: -2px;
+left: 600px;
+right: 50px;
+
+`;
+
+
 
 const theme = createMuiTheme({
   typography: {
@@ -50,10 +64,11 @@ export class MessageForm extends Component {
             defaultValue={values.message}
           />
           <br />
-
+          <FormButton2>
           <Button variant="contained" color="secondary" onClick={this.back}>
             Back
           </Button>
+          </FormButton2>
           <FormButton>
             <Button variant="contained" color="primary" onClick={this.continue}>
               Submit
