438c7e5295c456209385e1953d9af9135afd709f
Astrid <astrid.sinabian@chasacademy.se>
2019-05-28 12:40:57 +0200

    Add small changes to styling for about and made responsive

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -3,25 +3,41 @@ import styled from "styled-components";
 import Header from "../layout/Header";
 import Footer from "../layout/Footer";
 
-
 const Body = styled.div`
+  @import url(https://fonts.googleapis.com/css?family=Didact+Gothic);
   max-width: 980px;
   min-height: 80vh;
   margin: 30px auto;
   height: 100%;
   width: 100%;
+  font-family: 'Didact Gothic', sans-serif;
 `;
 
 const StyleHeader1 = styled.h1`
-color: #55273c;
+color: #616161;
 font-size: 40px;
-text-align: center;;
+text-align: center;
+  
+@media (min-width: 320px) and (max-width: 480px) {
+  font-size: 32px;
+}
+
+
 `;
 
 const StyleParagraph = styled.p`
 font-size: 20px;
-color: #55273c;
+color: gray;
 text-align: center;
+
+@media (min-width: 320px) and (max-width: 480px) {
+  padding: 5px 60px 10px 60px;
+  font-size: 16px;
+}
+
+@media (min-width: 768px) and (max-width: 1024px) {
+ padding: 15px 100px 10px 100px;
+}
 `
 
 function About () {
@@ -29,7 +45,7 @@ function About () {
     <div>
       <Header />
       <Body>
-      <StyleHeader1>-Our Mission-</StyleHeader1>
+      <StyleHeader1>Our Mission</StyleHeader1>
 
       <StyleParagraph>
       Welcome to Formation, the world leading platform for showcasing your resume or scouting for talents. We provide you with seamless interface, easily adjustable profiles and multiple ways for employers and employees to meet. Our unique algorithm makes sure that you will find exactly what you are looking for, without waiting times or scrolling through useless search results. Try us out today - you won’t be looking back. 
