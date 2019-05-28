dd217f8a297fc2ff54305c7726b98ef7a4d96419
Astrid <astrid.sinabian@chasacademy.se>
2019-05-28 20:34:12 +0200

    Add responsive design to Welcome page

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -22,20 +22,25 @@ const BackgroundImg = styled.div`
   background-repeat: no-repeat;
   background-size: cover;
   width: 100%;
-  height: 800px;
+  height: 965px;
 `;
 
 const Logo = styled.div`
   display: flex;
   justify-content: center;
   height: 60px;
-  padding: 20px 0 5px 8px;
+  padding: 30px 0 5px 8px;
 `;
 
 const Img = styled.img`
   width: 70px;
   height: 70px;
   border-radius: 50%;
+
+  @media (min-width: 320px) and (max-width: 480px) {
+    width: 40px;
+    height: 40px;
+  }
 `;
 
 const StyledLink = styled(Link)`
@@ -51,6 +56,13 @@ const StyledLink = styled(Link)`
     color: #bb8fa9;
     text-decoration: none;
   }
+
+  @media (min-width: 320px) and (max-width: 480px) {
+    font-size: 11px;
+    padding: 14px 10px 0 10px;
+    text-align: center;
+  }
+  
 `;
 
 const Line = styled.div`
@@ -58,6 +70,17 @@ const Line = styled.div`
   justify-content: center;
   padding-top: 40px;
   color: lightgray;
+  text-align: center;
+
+  @media (min-width: 320px) and (max-width: 480px) {
+    padding-top: 16px;
+    font-size: 6px;
+  }
+
+  @media (min-width: 481px) and (max-width: 767px) {
+    padding-top: 60px;
+    font-size: 8px;
+  }
 `;
 
 const Main = styled.div`
@@ -73,12 +96,36 @@ const HeaderOne = styled.h1`
   text-align: center;
   color: gray;
   margin: 2.5em 0 0.5em 0;
+
+  @media (min-width: 320px) and (max-width: 480px) {
+    font-size: 18px;
+    font-weight: bold;
+    margin-top: 1.7em;
+  }
+
+  @media (min-width: 481px) and (max-width: 767px) {
+    font-size: 22px;
+    font-weight: bold;
+    margin-top: 1.7em;
+  }
 `;
 
 const HeaderThree = styled.h3`
   text-align: center;
   color: #a09f9f;
   margin: 0 0 0.5em 0;
+
+  @media (min-width: 320px) and (max-width: 480px) {
+    font-size: 15px;
+  }
+
+  @media (min-width: 481px) and (max-width: 767px) {
+    font-size: 15px;
+  }
+
+  @media (min-width: 767px) and (max-width: 1024px) {
+    font-size: 24px;
+  }
 `;
 
 const GetStarted = styled.div`
@@ -115,14 +162,60 @@ const MyNavLink = styled(NavLink)`
     visibility: visible;
     width: 100%;
   }
+
+  @media (min-width: 320px) and (max-width: 480px) {
+    margin: 0.5em 5em 0 5em;
+    font-size: 14px;
+    border: 1px solid #bb8fa9;
+    padding: 10px;
+    margin: 35px 110px 18px 110px;
+
+  }
+
+  @media (min-width: 481px) and (max-width: 767px) {
+    margin: 0.7em 10em 0 10em;
+    font-size: 18px;
+    padding: 10px;
+  }
+
+  @media (min-width: 767px) and (max-width: 1024px) {
+    margin: 0.7em 12em 0 12em;
+    font-size: 25px;
+    padding: 10px;
+  }
+
+  @media (min-width: 1025px) and (max-width: 1280px) {
+    margin: 0.7em 15em 0 15em;
+    font-size: 28px;
+    padding: 10px;
+  }
 `;
 
 const Circles = styled.div`
   display: flex;
   justify-content: center;
-  align-item: center;
+  align-items: center;
   text-align: center;
   margin-top: 2em;
+
+  @media (min-width: 320px) and (max-width: 480px) {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 0;
+  }
+
+  @media (min-width: 481px) and (max-width: 767px) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-flow: wrap;
+    margin-top: 20px;
+  }
+
+  @media (min-width: 1025px) and (max-width: 1280px) {
+    margin-top: 50px;
+  }
 `;
 
 const Circle = styled.div`
@@ -135,11 +228,41 @@ const Circle = styled.div`
   height: 200px;
   margin: 10px;
   padding: 30px;
+
+  @media (min-width: 320px) and (max-width: 480px) {
+    background-color: #ffffff00;
+    width: 280px;
+    height: 100px;
+    border-radius: 5px;
+    padding: 10px;
+    margin: 10px 40px;
+  }
+
+  @media (min-width: 481px) and (max-width: 569px) {
+    width: 170px;
+    height: 170px;
+    margin: 10px;
+    padding: 20px;
+  }
+
+  @media (min-width: 481px) and (max-width: 767px) {
+    width: 200px;
+    height: 200px;
+    margin: 10px;
+    padding: 30px;
+    font-size: 12px;
+  }
+
 `;
 
 const MyFontAwesomeIcon = styled(FontAwesomeIcon)`
   font-size: 20px;
   margin-bottom: 17px;
+
+  @media (min-width: 320px) and (max-width: 480px) {
+    font-size: 18px;
+    margin: 10px 0;
+  }
 `;
 
 const WelcomeFooter = styled.footer`
@@ -150,6 +273,28 @@ const WelcomeFooter = styled.footer`
   padding-top: 60px;
   font-size: 8px;
   color: #e7e2e5;
+
+  @media (min-width: 320px) and (max-width: 480px) {
+    padding-top: 30em;
+  }
+
+  @media (min-width: 481px) and (max-width: 767px) {
+    color: #ffffff00;
+    padding: 0;
+  }
+
+  @media (min-width: 767px) and (max-width: 1024px) {
+    padding-top: 30em;
+  }
+
+  @media (min-width: 1025px) and (max-width: 1280px) {
+    padding-top: 25em;
+  }
+
+  @media (min-width: 1281px) {
+    padding-top: 28em;
+  }
+  
 `;
 
 function Welcome() {
