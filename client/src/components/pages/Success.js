4a4073cd0a9001a408e7c7fc788a816fb7b97362
Diego Pinones <pinones.diego@gmail.com>
2019-05-29 22:27:06 +0200

    added home button on last page in contact

--- client/src/components/pages/Success.js
+++ client/src/components/pages/Success.js
@@ -3,6 +3,9 @@ import { MuiThemeProvider, createMuiTheme } from "@material-ui/core";
 import Header from "../layout/Header";
 import styled from "styled-components";
 import Footer from "../layout/Footer"
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowAltCircleLeft } from "@fortawesome/free-solid-svg-icons";
 const theme = createMuiTheme({
   typography: {
     useNextVariants: true
@@ -26,6 +29,44 @@ width: 100%;
 height: 10rem;
 `
 
+const Buttons = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledLink = styled(Link)`
+cursor: pointer;
+font: inherit;
+padding: 0.5em 1em;
+border-radius: 2em;
+text-decoration: none;
+background-color: #bb8fa9;
+color: #fafafa;
+text-align: center;
+transition: all 0.2s;
+border: none;
+
+:hover {
+  background-color: #d8c1cf;
+  text-decoration: none;
+  color: white;
+}
+
+  @media (min-width: 320px) and (max-width: 480px) {
+    margin: 0.5em 0.3em 0.3em 0;
+  }
+
+  @media (min-width: 768px) and (max-width: 1024px) {
+    margin: 1em 0.3em 0.3em 0;
+  }
+
+  @media (min-width: 1025px) {
+    margin: 1em 0.3em 0.3em 0;
+  }
+
+`;
+
 export class Success extends Component {
   render() {
     return (
@@ -36,6 +77,11 @@ export class Success extends Component {
           <StyleParagraph>
             We will respond to you as soon as possible
           </StyleParagraph>
+
+
+          <Buttons>
+          <StyledLink to="/"><FontAwesomeIcon icon={faArrowAltCircleLeft} /> Home</StyledLink>
+          </Buttons>
           <StyleFooter>
           <Footer />
           </StyleFooter>
